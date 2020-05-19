bcb6ebead41591178b1e39db35b95943b67e8985
Harith Onigemo <45686269+Harithmetic1@users.noreply.github.com>
2020-05-20 00:29:26 +0100

    Update Navbar.js

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -4,6 +4,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-dark bg-success mb-5">
             <span className="navbar-brand mb-0 h1 mx-auto">MusicMatch</span>
+        <a href="https://github.com/Harithmetic1/" className="text-light"><i class="fab fa-github"></i></a>
         </nav>
     )
 }
